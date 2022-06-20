e901a891e2f4669a6bf39fadd6bfd873404a7779
jahnx <jahn739@gmail.com>
2022-06-20 11:59:37 +0900

    Cors설정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import * as config from 'config';
 const serverConfig = config.get('server');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    credentials: true,
+  });
   await app.listen(`${serverConfig.port}`);
 }
 bootstrap();
